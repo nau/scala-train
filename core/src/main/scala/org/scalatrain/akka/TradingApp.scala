f3379ff9eec397e830e707ae9a66751036c9ccf3
Alexander Nemish <anemish@gmail.com>
2015-06-25 17:44:32 +0300

    Remove trading app implementation.

--- core/src/main/scala/org/scalatrain/akka/TradingApp.scala
+++ core/src/main/scala/org/scalatrain/akka/TradingApp.scala
@@ -3,6 +3,7 @@ package org.scalatrain.akka
 import akka.actor.SupervisorStrategy.{Escalate, Stop, Restart, Resume}
 import akka.actor._
 import akka.pattern._
+import akka.routing._
 import akka.util.Timeout
 
 import scala.collection.mutable
@@ -55,12 +56,60 @@ class TradingActor extends Actor {
     case _: Exception => Escalate
   }
 
+
+  /**
+   * akka.routing.RoundRobinRoutingLogic
+   * akka.routing.RandomRoutingLogic
+   * akka.routing.SmallestMailboxRoutingLogic
+   * akka.routing.BroadcastRoutingLogic
+   * akka.routing.ScatterGatherFirstCompletedRoutingLogic
+   * akka.routing.TailChoppingRoutingLogic
+   * akka.routing.ConsistentHashingRoutingLogic
+   */
+  var router = {
+    val routees = Vector.fill(5) {
+      val r = context.actorOf(Props[ServiceActor])
+      context watch r
+      ActorRefRoutee(r)
+    }
+    Router(ScatterGatherFirstCompletedRoutingLogic(10.seconds), routees)
+  }
+
+  /**
+   * Router Actor
+   *
+   * Pool - The router creates routees as child actors and removes them from the router
+   * if they terminate.
+   *
+   * Group - The routee actors are created externally to the router
+   * and the router sends messages to the specified path using actor selection,
+   * without watching for termination.
+   */
+  val router2: ActorRef =
+    context.actorOf(RoundRobinPool(5).props(Props[ServiceActor]), "router2")
+
+  /**
+    * Group - The routee actors are created externally to the router
+    * and the router sends messages to the specified path using actor selection,
+    * without watching for termination.
+    */
+  val router3: ActorRef =
+    context.actorOf(RoundRobinGroup(List(service1.path.toString, service2.path.toString)).props(), "router4")
+
   def receive = {
     case t: Trade =>
       val p = process(t)
+
       val f = Future.firstCompletedOf(Seq(service1 ? p, service2 ? p))
       val s = sender()
       f onComplete(r => s ! r)
+
+      router.route(t, sender())
+    case Terminated(a) =>
+      router = router.removeRoutee(a)
+      val r = context.actorOf(Props[ServiceActor])
+      context watch r
+      router = router.addRoutee(r)
   }
 }
 
